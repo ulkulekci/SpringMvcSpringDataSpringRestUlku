bc72ffdcf15f59571b1a7b589a6a4a643fd53513
ulkulekci <ulkusulu07@hotmail.com>
2022-02-15 21:29:18 +0300

    Initial commit

---
+++ src/main/java/com/innova/springmvcspringdataspringrestulku/SpringMvcSpringDataSpringRestUlkuApplication.java
@@ -0,0 +1,13 @@
+package com.innova.springmvcspringdataspringrestulku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSpringDataSpringRestUlkuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcSpringDataSpringRestUlkuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innova/springmvcspringdataspringrestulku/SpringMvcSpringDataSpringRestUlkuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innova.springmvcspringdataspringrestulku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSpringDataSpringRestUlkuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
